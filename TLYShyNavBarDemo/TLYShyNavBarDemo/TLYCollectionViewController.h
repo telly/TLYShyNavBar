ff67d2b0a084f816d8060576134389c90b64a994
Mazyad Alabduljaleel <mazjaleel@gmail.com>
2015-10-20 19:41:36 -0700

    Added collectionView demo Has the problem described in #93

---
+++ TLYShyNavBarDemo/TLYShyNavBarDemo/TLYCollectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  TLYCollectionViewController.h
+//  TLYShyNavBarDemo
+//
+//  Created by Mazyad Alabduljaleel on 10/20/15.
+//  Copyright © 2015 Telly, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TLYCollectionViewController : UICollectionViewController
+
+@end
