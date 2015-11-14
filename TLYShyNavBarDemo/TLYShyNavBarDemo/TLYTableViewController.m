630032648b29b1004f2040b7f3e2f2764e94c58a
Maz Jaleel <mazjaleel@gmail.com>
2015-11-13 18:23:38 -0800

    Added TableView in demo. Time to fix it!

---
+++ TLYShyNavBarDemo/TLYShyNavBarDemo/TLYTableViewController.m
@@ -0,0 +1,62 @@
+//
+//  TLYTableViewController.m
+//  TLYShyNavBarDemo
+//
+//  Created by Mazyad Alabduljaleel on 11/13/15.
+//  Copyright © 2015 Telly, Inc. All rights reserved.
+//
+
+#import "TLYTableViewController.h"
+
+@interface TLYTableViewController () <UITableViewDataSource, UITableViewDelegate>
+
+@property (nonatomic, weak) IBOutlet UITableView *tableView;
+
+@end
+
+@implementation TLYTableViewController
+
+#pragma mark - View Life Cycle
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    UIView *view = view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, CGRectGetWidth(self.view.bounds), 40.f)];
+    view.backgroundColor = [UIColor redColor];
+    
+    
+    /* Library code */
+    self.shyNavBarManager.scrollView = self.tableView;
+    /* Can then be remove by setting the ExtensionView to nil */
+    [self.shyNavBarManager setExtensionView:view];
+}
+
+#pragma mark - UITableViewDataSource
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 6;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 5;
+}
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
+{
+    return @"Section Header";
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell"
+                                                            forIndexPath:indexPath];
+    
+    cell.textLabel.text = @"Sample Data";
+    
+    return cell;
+}
+
+@end
