58a2d1b31cb87756a5a9301ec12c91a1b5abb5cd
Steven Zweier <steven@percolate.com>
2016-02-23 09:50:29 -0500

    adjust alignment

--- TLYShyNavBar/ShyControllers/TLYShyScrollViewController.m
+++ TLYShyNavBar/ShyControllers/TLYShyScrollViewController.m
@@ -20,18 +20,18 @@
 - (CGFloat)updateLayoutIfNeeded
 {
     if (self.scrollView.contentSize.height < FLT_EPSILON
-         && ([self.scrollView isKindOfClass:[UITableView class]]
-             || [self.scrollView isKindOfClass:[UICollectionView class]])
-         )
+        && ([self.scrollView isKindOfClass:[UITableView class]]
+            || [self.scrollView isKindOfClass:[UICollectionView class]])
+        )
     {
         return 0.f;
     }
-    
+
     CGFloat parentMaxY = [self.parent maxYRelativeToView:self.scrollView.superview];
     CGFloat normalizedY = parentMaxY - self.scrollView.frame.origin.y;
     UIEdgeInsets insets = UIEdgeInsetsMake(self.scrollView.contentInset.top, 0, self.scrollView.contentInset.bottom, 0);
     insets.top = normalizedY;
-    
+
     if (normalizedY > -FLT_EPSILON && !UIEdgeInsetsEqualToEdgeInsets(insets, self.scrollView.contentInset))
     {
         CGFloat delta = insets.top - self.scrollView.contentInset.top;
@@ -39,19 +39,19 @@
         if (self.refreshControl == nil || [self.refreshControl isHidden]) {
             [self.scrollView tly_setInsets:insets];
         }
-        
+
         return delta;
     }
-    
+
     if (normalizedY < -FLT_EPSILON)
-    {        
+    {
         CGRect frame = self.scrollView.frame;
         frame = UIEdgeInsetsInsetRect(frame, insets);
-        
+
         self.scrollView.frame = frame;
         return [self updateLayoutIfNeeded];
     }
-    
+
     return 0.f;
 }
 
