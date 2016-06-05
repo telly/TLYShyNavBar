371124a501fa14c741c91e14fb370772102943d9
ndrewh <ndrewh@users.noreply.github.com>
2016-06-05 19:27:35 -0400

    Fix calculateTotalHeightRecursively mid-transition
    There was an issue where TLYShyNavBar would start contracting, but before completely retracted would decide there wasn't enough content left to show. This updates calculateTotalHeightRecursively to return the proper value mid transition.

--- TLYShyNavBar/ShyControllers/TLYShyViewController.m
+++ TLYShyNavBar/ShyControllers/TLYShyViewController.m
@@ -21,7 +21,11 @@
 
 - (CGFloat)calculateTotalHeightRecursively
 {
-    return CGRectGetHeight(self.view.bounds) + [self.parent calculateTotalHeightRecursively];
+    float overlap = [self.parent maxYRelativeToView:self.view];
+    if ([self.parent isMemberOfClass:[TLYShyStatusBarController class]] && overlap - self.view.frame.origin.y > 0) {
+        overlap += overlap - self.view.frame.origin.y;
+    }
+    return CGRectGetHeight(self.view.bounds) - overlap + [self.parent calculateTotalHeightRecursively];
 }
 
 @end
