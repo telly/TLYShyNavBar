a8e271cf4ad26bbda73806dd65b959c2691095f6
Evan D. Schoenberg, M.D <tekjew@gmail.com>
2016-09-08 03:53:26 -0400

    Add iPad support w/ popover test to the demo (#153)
    * The demo now supports iPad and has an option to show a popover
    
    * Remove debugging purple coloration from the new iPad popover demo

---
+++ TLYShyNavBarDemo/TLYShyNavBarDemo/TLYPopoverSegue.h
@@ -0,0 +1,13 @@
+//
+//  TLYPopoverSegue.h
+//  TLYShyNavBarDemo
+//
+//  Created by Evan Schoenberg on 8/23/16.
+//  Copyright © 2016 Telly, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TLYPopoverSegue : UIStoryboardSegue
+
+@end
